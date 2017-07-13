74614550df00e841139c5c4631a01b2a9412ad93
Reyn-L <Reyn.Leong@gmail.com>
2017-07-12 15:21:39 -1000

    58

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1054,7 +1054,11 @@ class Lightbulb {
  * Cookie class
  * @param {string} flavor The cookie's flavor
  */
-
+class Cookie {
+  constructor(flavor) {
+    this.flavor = flavor;
+  }
+}
 
 /**
  * Step 59
