1afd1e1d7f65b98e7bcd2ff3caa1f83e54dfd5a1
Reyn-L <Reyn.Leong@gmail.com>
2017-07-12 15:29:35 -1000

    61

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1079,7 +1079,7 @@ Scientist.prototype.addDiscovery = function(discovery) {
 
 // Create 2 different species of animals
 var george = new Animal("Monkey", 'male');
-var nemo;
+var nemo = new Animal("Fish", "male");
 
 // Create 2 different vehicles
 var civic;
